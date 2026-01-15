38ba402bd61ad75d0ab1a5038613f1943e72e051
mininartur604-cmd <mininartur604@gmail.com>
2026-01-15 11:37:16 +1000

    Add files via upload

--- footer.js
+++ footer.js
@@ -1,1 +1,19 @@
 console.log("ДОбро пожаловать");
+let number1 = 10;
+let number2 = 10;
+let sum = number1 + number2;
+console.log(sum);
+console.log(number1-number2);
+console.log(number1*number2);
+if (number2 == 0)
+{
+    console.log("На ноль делить нельзя")
+}
+else
+{
+    console.log(number1/number2);
+}
+for (let i = 1,; i < 5 ; i++);
+{
+    console.log(i)
+}
